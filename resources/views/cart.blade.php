2148e27623d91bece43c94f6a497360c431c5825
viettrungq92 <92778864+viettrungq92@users.noreply.github.com>
2025-01-07 20:16:13 +0700

    Update Cart Quantity
    Update Cart Quantity

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -59,17 +59,26 @@
                   </div>
                 </td>
                 <td>
-                  <span class="shopping-cart__product-price">{{$item->price}}</span>
+                  <span class="shopping-cart__product-price">${{$item->price}}</span>
                 </td>
                 <td>
                   <div class="qty-control position-relative">
                     <input type="number" name="quantity" value="{{$item->qty}}" min="1" class="qty-control__number text-center">
-                    <div class="qty-control__reduce">-</div>
-                    <div class="qty-control__increase">+</div>
+                    <form method="POST" action="{{route('cart.qty.decrease',['rowId'=>$item->rowId])}}" >
+                        @csrf
+                        @method('PUT')
+                        <div class="qty-control__reduce">-</div>
+                    </form>
+                    <form method="POST" action="{{route('cart.qty.increase',['rowId'=>$item->rowId])}}" >
+                        @csrf
+                        @method('PUT')
+                        <div class="qty-control__increase">+</div>
+                    </form>
+                    
                   </div>
                 </td>
                 <td>
-                  <span class="shopping-cart__subtotal">{{$item->subTotal()}}</span>
+                  <span class="shopping-cart__subtotal">${{$item->subTotal()}}</span>
                 </td>
                 <td>
                   <a href="#" class="remove-cart">
@@ -139,3 +148,18 @@
     </section>
   </main>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function(){
+            $(".qty-control__increase").on("click",function(){
+                $(this).closest('form').submit();
+            });
+
+            $(".qty-control__reduce").on("click",function(){
+                $(this).closest('form').submit();
+            });
+        })
+    </script>
+
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/shop/{product_slug}',[ShopController::class,'product_details'])->na
 
 Route::get('/cart',[CartController::class,'index'])->name('cart.index');
 Route::post('/cart/add',[CartController::class,'add_to_cart'])->name('cart.add');
+Route::put('cart/increase-quantity/{rowId}',[CartController::class,'increase_cart_quantity'])->name('cart.qty.increase');
+Route::put('cart/decrease-quantity/{rowId}',[CartController::class,'decrease_cart_quantity'])->name('cart.qty.decrease');
 
 
 Route::middleware(['auth'])->group(function () {
