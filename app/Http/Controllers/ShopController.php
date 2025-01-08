d3f821fd5e8f880324efc750ec094d7bec6cd441
viettrungq92 <92778864+viettrungq92@users.noreply.github.com>
2025-01-08 15:26:21 +0700

    Product Sorting and Products Per Page
    Product Sorting and Products Per Page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,9 +8,35 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index(){
-        $products = Product::orderBy('created_at','DESC')->paginate(12);
-        return view('shop', compact('products'));
+    public function index(Request $request){
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+        switch($order)
+        {
+            case 1:
+                $o_column = "created_at";
+                $o_order = "DESC";
+                break;
+            case 2:
+                $o_column = "created_at";
+                $o_order = "ASC";
+                break;
+            case 3:
+                $o_column = "sale_price";
+                $o_order = "ASC";
+                break;
+            case 4:
+                $o_column = "sale_price";
+                $o_order = "DESC";
+                break;
+            default:
+                $o_column = "id";
+                $o_order = "DESC";
+        }
+        $products = Product::orderBy($o_column,$o_order)->paginate($size);
+        return view('shop', compact('products','size','order'));
     }
 
     public function product_details($product_slug){
