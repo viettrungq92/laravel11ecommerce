9545efad009fb3623a37e2455ccf9c7bb87b5989
viettrungq92 <92778864+viettrungq92@users.noreply.github.com>
2025-01-09 01:26:16 +0700

    Products Filter By Price
    Products Filter By Price

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -181,16 +181,16 @@
             </h5>
             <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
               aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
-              <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="10"
-                data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]" data-currency="$" />
+              <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="1"
+                data-slider-max="500" data-slider-step="5" data-slider-value="[{{$min_price}},{{$max_price}}]" data-currency="$" />
               <div class="price-range__info d-flex align-items-center mt-2">
                 <div class="me-auto">
                   <span class="text-secondary">Min Price: </span>
-                  <span class="price-range__min">$250</span>
+                  <span class="price-range__min">$1</span>
                 </div>
                 <div>
                   <span class="text-secondary">Max Price: </span>
-                  <span class="price-range__max">$450</span>
+                  <span class="price-range__max">$500</span>
                 </div>
               </div>
             </div>
@@ -436,6 +436,8 @@
     <input type="hidden" name="order" id="order" value="{{$order}}" />
     <input type="hidden" name="brands" id="hdnBrands" />
     <input type="hidden" name="categories" id="hdnCategories" />
+    <input type="hidden" name="min" id="hdnMinPrice" value="{{$min_price}}"/>
+    <input type="hidden" name="max" id="hdnMaxPrice" value="{{$max_price}}"/>
   </form>
 
 @endsection
@@ -481,6 +483,16 @@
         });
         $("#hdnCategories").val(categories);
         $("#frmfilter").submit();
+      });
+
+      $("[name='price_range']").on("change",function(){
+        var min = $(this).val().split(',')[0];
+        var max = $(this).val().split(',')[1];
+        $("#hdnMinPrice").val(min);
+        $("#hdnMaxPrice").val(max);
+        setTimeout(()=> {
+          $("#frmfilter").submit();
+        }, 2000);
       });
     });
   </script>
