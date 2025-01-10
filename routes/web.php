b142f37da2bfce3791013696e1e0236dd468ae83
viettrungq92 <92778864+viettrungq92@users.noreply.github.com>
2025-01-10 09:24:10 +0700

    Move Product From Wishlist to Cart
    Move Product From Wishlist to Cart

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::post('/wishlist/add',[WishlistController::class,'add_to_wishlist'])->name
 Route::get('/wishlist',[WishlistController::class,'index'])->name('wishlist.index');
 Route::delete('/wishlist/item/remove/{rowId}',[WishlistController::class,'remove_item'])->name('wishlist.item.remove');
 Route::delete('/wishlist/clear',[WishlistController::class,'empty_wishlist'])->name('wishlist.items.clear');
+Route::post('/wishlist/move-to-cart/{rowId}',[WishlistController::class,'move_to_cart'])->name('wishlist.move.to.cart');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class,'index'])->name('user.index');
